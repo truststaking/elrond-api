7614706dab0f2deb93cd6974fdffbd37e0d7668f
Constantin <constantin.tovisi@elrond.com>
2021-01-06 19:40:02 +0200

    nodes routes

--- handlers/index.js
+++ handlers/index.js
@@ -1,9 +1,11 @@
 const { handler: constantsHandler } = require('./constants');
 const { handler: elasticSearchHandler } = require('./elasticSearch');
+const { handler: nodesHandler } = require('./nodes');
 const { handler: tokensHandler } = require('./tokens');
 
 module.exports = {
   constantsHandler,
   elasticSearchHandler,
+  nodesHandler,
   tokensHandler,
 };
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const indexRouter = require('./indexRouter');
 const constantsRouter = require('./constantsRouter');
 const elasticSearchRouter = require('./elasticSearchRouter');
+const nodesRouter = require('./nodesRouter');
 const tokensRouter = require('./tokensRouter');
 
 module.exports = {
   indexRouter,
   constantsRouter,
   elasticSearchRouter,
+  nodesRouter,
   tokensRouter,
 };

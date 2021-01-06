1fffca08ff6a65e12c0de78dcce88c740f82b7ee
Constantin <constantin.tovisi@elrond.com>
2021-01-06 18:39:39 +0200

    tokens endpoints

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,11 @@
+const getTokenProperties = require('./getTokenProperties');
 const redis = require('./redis');
 const response = require('./response');
+const vmQuery = require('./vmQuery');
 
 module.exports = {
+  getTokenProperties,
   redis,
   response,
+  vmQuery,
 };

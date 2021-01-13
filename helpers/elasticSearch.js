79df1f21b570d75e85d77ae3228ebf875290f5d6
Constantin <constantin.tovisi@elrond.com>
2021-01-13 19:01:15 +0200

    elastic search helper

--- helpers/elasticSearch.js
+++ helpers/elasticSearch.js
@@ -1,48 +1,75 @@
 const axios = require('axios');
 const { elasticUrl } = require('../config');
 
-const buildParams = ({ query, sort = false, order = false }) => {
-  return { query: { match_all: {} } };
+const buildQuery = (query = {}) => {
+  delete query.from;
+  delete query.size;
+
+  if (Object.keys(query).length) {
+    const must = Object.keys(query).map((key) => {
+      const match = {};
+      match[key] = query[key];
+
+      return { match };
+    });
+
+    query = { bool: { must } };
+  } else {
+    query = { match_all: {} };
+  }
+
+  return query;
 };
 
-const formatItem = ({ key, document, fields }) => {
+const buildSort = (sort = {}) => {
+  sort = Object.keys(sort).map((key) => {
+    const obj = {};
+    obj[key] = {};
+    obj[key].order = sort[key];
+
+    return obj;
+  });
+
+  return sort;
+};
+
+const formatItem = ({ document, key }) => {
   const { _id, _source } = document;
   const item = {};
   item[key] = _id;
+
   return { ...item, ..._source };
 };
 
-const getList = async ({ collection, key, query, sort, order }) => {
-  // console.log(collection, key, query, sort, order);
+const getList = async ({ collection, key, query, sort }) => {
   const url = `${elasticUrl()}/${collection}/_search`;
-  const params = buildParams({ query, sort, order });
+  const { from = 0, size = 25 } = query;
+  query = buildQuery(query);
+  sort = buildSort(sort);
 
   const {
     data: {
       hits: { hits: documents },
     },
-  } = await axios.post(url, params);
+  } = await axios.post(url, { query, sort, from, size });
 
-  return documents.map((document) => formatItem({ key, document }));
+  return documents.map((document) => formatItem({ document, key }));
 };
 
-const getItem = async ({ collection, key, hash, fields }) => {
-  // console.log(collection, key, hash, fields);
+const getItem = async ({ collection, key, hash }) => {
   const url = `${elasticUrl()}/${collection}/_doc/${hash}`;
-
   const { data: document } = await axios.get(url);
 
-  return formatItem({ key, document, fields });
+  return formatItem({ document, key });
 };
 
 const getCount = async ({ collection, query }) => {
-  // console.log(collection, query);
   const url = `${elasticUrl()}/${collection}/_count`;
-  const params = buildParams(query);
+  query = buildQuery(query);
 
   const {
     data: { count },
-  } = await axios.post(url, params);
+  } = await axios.post(url, { query });
 
   return count;
 };

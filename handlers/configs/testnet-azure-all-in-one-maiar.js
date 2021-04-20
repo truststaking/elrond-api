6e1e87a868c0d75d4cd4e7944a5d531340253480
pislaProgramming <alex.pislariu2000@gmail.com>
2021-04-20 18:13:43 +0300

    Client configs

--- handlers/configs/testnet-azure-all-in-one-maiar.js
+++ handlers/configs/testnet-azure-all-in-one-maiar.js
@@ -1,13 +1,10 @@
-const configs = require('./config');
 const id = 'testnet-azure-all-in-one-maiar';
 const name = 'Maiar API All-In-One Testnet';
 const adapter = 'elastic';
 const erdLabel = 'XeGLD';
 const theme = 'testnet';
-const apiUrl = 'https://testnet-api.elrond.com';
 const elasticUrls = ['https://elastic-maiar.elrond.com'];
 const gatewayUrls = ['https://proxy-maiar.elrond.com'];
-const providersUrl = 'https://delegation.maiarbrowser.com/providers';
 
 const elasticUrl = () => {
   return elasticUrls[Math.floor(Math.random() * elasticUrls.length)];
@@ -17,12 +14,6 @@ const gatewayUrl = () => {
   return gatewayUrls[Math.floor(Math.random() * gatewayUrls.length)];
 };
 
-const vmQueryUrl = () => {
-  return gatewayUrl();
-};
-
-const network = 'testnet';
-
 module.exports = {
   id,
   name,
@@ -31,9 +22,4 @@ module.exports = {
   theme,
   gatewayUrl,
   elasticUrl,
-  vmQueryUrl,
-  apiUrl,
-  providersUrl,
-  network,
-  ...configs,
 };
--- handlers/configs/testnet-azure-all-in-one.js
+++ handlers/configs/testnet-azure-all-in-one.js
@@ -1,13 +1,11 @@
-const configs = require('./config');
 const id = 'testnet-azure-all-in-one';
 const name = 'Azure Maiar Dev Testnet';
 const adapter = 'elastic';
 const erdLabel = 'XeGLD';
 const theme = 'testnet';
-const apiUrl = 'https://testnet-api.elrond.com';
+
 const elasticUrls = ['http://35.207.191.245'];
 const gatewayUrls = ['http://35.207.191.245:8080'];
-const providersUrl = 'https://delegation.maiarbrowser.com/providers';
 
 const elasticUrl = () => {
   return elasticUrls[Math.floor(Math.random() * elasticUrls.length)];
@@ -17,12 +15,6 @@ const gatewayUrl = () => {
   return gatewayUrls[Math.floor(Math.random() * gatewayUrls.length)];
 };
 
-const vmQueryUrl = () => {
-  return gatewayUrl();
-};
-
-const network = 'testnet';
-
 module.exports = {
   id,
   name,
@@ -31,9 +23,4 @@ module.exports = {
   theme,
   gatewayUrl,
   elasticUrl,
-  vmQueryUrl,
-  apiUrl,
-  providersUrl,
-  network,
-  ...configs,
 };

9740d0e0db55f28db159f0bd9e849a448982ed94
pislaProgramming <alex.pislariu2000@gmail.com>
2021-04-20 18:13:43 +0300

    Client configs

--- handlers/configs/testnet-do-fra.js
+++ handlers/configs/testnet-do-fra.js
@@ -1,13 +1,11 @@
-const configs = require('./config');
 const id = 'testnet-do-fra';
 const name = 'DigitalOcean Frankfurt Testnet';
 const adapter = 'elastic';
 const erdLabel = 'XeGLD';
 const theme = 'testnet';
-const apiUrl = 'https://testnet-api.elrond.com';
+
 const elasticUrls = ['http://104.248.103.160'];
 const gatewayUrls = ['http://144.126.246.1:8080'];
-const providersUrl = 'https://delegation.maiarbrowser.com/providers';
 
 const elasticUrl = () => {
   return elasticUrls[Math.floor(Math.random() * elasticUrls.length)];
@@ -17,12 +15,6 @@ const gatewayUrl = () => {
   return gatewayUrls[Math.floor(Math.random() * gatewayUrls.length)];
 };
 
-const vmQueryUrl = () => {
-  return gatewayUrl();
-};
-
-const network = 'testnet';
-
 module.exports = {
   id,
   name,
@@ -31,9 +23,4 @@ module.exports = {
   theme,
   gatewayUrl,
   elasticUrl,
-  vmQueryUrl,
-  apiUrl,
-  providersUrl,
-  network,
-  ...configs,
 };
--- handlers/configs/testnet-ibm-multi.js
+++ handlers/configs/testnet-ibm-multi.js
@@ -1,13 +1,11 @@
-const configs = require('./config');
 const id = 'testnet-ibm-multi';
 const name = 'IBM MULTI Testnet';
 const adapter = 'elastic';
 const erdLabel = 'XeGLD';
 const theme = 'testnet';
-const apiUrl = 'https://testnet-api.elrond.com';
+
 const elasticUrls = ['http://158.175.177.253'];
 const gatewayUrls = ['http://158.175.191.253:8080'];
-const providersUrl = 'https://delegation.maiarbrowser.com/providers';
 
 const elasticUrl = () => {
   return elasticUrls[Math.floor(Math.random() * elasticUrls.length)];
@@ -17,12 +15,6 @@ const gatewayUrl = () => {
   return gatewayUrls[Math.floor(Math.random() * gatewayUrls.length)];
 };
 
-const vmQueryUrl = () => {
-  return gatewayUrl();
-};
-
-const network = 'testnet';
-
 module.exports = {
   id,
   name,
@@ -31,9 +23,4 @@ module.exports = {
   theme,
   gatewayUrl,
   elasticUrl,
-  vmQueryUrl,
-  apiUrl,
-  providersUrl,
-  network,
-  ...configs,
 };

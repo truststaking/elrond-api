df35835178c086dd98df9bacd8041cc38a0f6653
Mihai Eremia <jobs@dani.ro>
2021-06-01 12:12:28 +0100

    Add SSL

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+var http = require('http');
+var https = require('https');
 const cors = require('cors');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 const { metricsApp, metricsRequestMiddleware, metricsPort } = require('./metrics');
 const { statuses } = require(`./handlers/configs/${process.env.CONFIG}`);
 const { prewarmHandler } = require('./handlers');
 
+var privateKey = fs.readFileSync('/home/ubuntu/server.key', 'utf8');
+var certificate = fs.readFileSync('/home/ubuntu/server.crt', 'utf8');
+var credentials = { key: privateKey, cert: certificate };
 const app = express();
 const port = 8000;
 
@@ -48,3 +54,9 @@ server.keepAliveTimeout = 61 * 1000; //61s
 server.headersTimeout = 65 * 1000; //65s `keepAliveTimeout + server's expected response time`
 
 const metricsServer = metricsApp.listen(metricsPort);
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(port);
+httpsServer.listen(8443);
